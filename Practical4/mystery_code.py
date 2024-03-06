e7f6ad2749d7adfbdb7d8ab467dfd1c2d90cc463
Nie Xinmiao <xinmiao.23@intl.zju.edu.cn>
2024-03-06 13:07:50 +0800

    m2

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,20 +1,20 @@
 # What does this piece of code do?
 # Answer: generate 100 random numbers between 1 and 10 and sum them
 
-# Import libraries 导入库
+# Import libraries
 # randint allows drawing a random number,
 # e.g. randint(1,5) draws a number between 1 and 5
 from random import randint
 
-# ceil takes the ceiling of a number, i.e. the next higher integer.生成这个数的下一个整数
+# ceil takes the ceiling of a number, i.e. the next higher integer.
 # e.g. ceil(4.2)=5
 from math import ceil
 
-progress=0
-total_random_number=0
-while progress<100:
-	progress+=1
-	n = randint(1,10)
-	total_random_number = total_random_number+n
+progress=0 #the initial count is zero
+total_random_number=0 #the initial total number is zero
+while progress<100: #total count is 0-99, is 100
+	progress+=1 #the number of progress in each loop is increased by one
+	n = randint(1,10) #generate a random number between 1 and 10
+	total_random_number = total_random_number+n #add up total_random_number and random number
 
 print(total_random_number)
